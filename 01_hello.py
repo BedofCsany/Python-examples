e44f6e7722d7d39e106ac34b7f3ddb5c7aab77f5
BedofCsany <79759596+BedofCsany@users.noreply.github.com>
2021-11-24 17:08:33 +0100

    1. fejezet

---
+++ 01_hello.py
@@ -0,0 +1,14 @@
+# A legelső Python programunk.
+# Figyeljük meg a print() függvény használatát!
+# Zárójelek kellenek!
+# A Helló világ azért van idézőjek között, mert az egy sztring (karaktersorozat).
+print("Helló Világ")
+# Így is jó.
+print('Helló Világ')
+# De így már nem lenne jó.
+#print('Helló Világ")
+# Szintaktikai hibát kapnánk.
+# Próbáld ki! Vedd ki a # jelet és futtasd úgy a programot.
+
+
+
---
+++ 01_print.py
@@ -0,0 +1,11 @@
+''' Egy ki nevű változóhoz hozzárendeljük az egyenlőségjel utáni
+kifejezésben kapott eredményt.'''
+ki = 6 + 4 * 9
+# Aztán kiírjuk a ki változó aktuális értékét.
+print (ki)
+# Ugyanezt az eredményt érhetjük el az alábbi módon is.
+print (6 + 4 * 9)
+''' A kifejezésben az értékek és a műveleti jelek közé azért teszünk szóközt,
+hogy átláthatóbb kódot kapjunk. '''
+input ("A befejezéshez, nyomja meg az ENTER-t!")
+
